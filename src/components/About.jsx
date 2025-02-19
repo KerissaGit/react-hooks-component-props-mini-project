23d631d85c64eb08bb043790e27cc415c900dd0e
KerissaGit <kerissa525@gmail.com>
2025-02-19 17:25:03 -0500

    All tests passing

---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+// placeholder image="https://via.placeholder.com/215"
+function About ({ image = "https://via.placeholder.com/215", about }) {
+    return (
+        <aside>
+            <img src={image} alt='blog logo'></img>
+            <p>{about}</p>
+        </aside>
+    )
+}
+
+export default About;
